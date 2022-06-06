6bb5fb18949d4fc4c49d6dfa6ae66197932e39e5
sarahjonna <100998930+sarahjonna@users.noreply.github.com>
2022-06-06 18:51:49 +0530

    Sample Project Structure

---
+++ Eve_Salon/src/main/java/com/eve/salon/EveSalonApplication.java
@@ -0,0 +1,13 @@
+package com.eve.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveSalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveSalonApplication.class, args);
+	}
+
+}
