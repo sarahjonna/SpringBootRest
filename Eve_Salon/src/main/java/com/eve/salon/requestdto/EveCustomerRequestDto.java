ce47e997a3d835bcc813c44eca61132473178d7e
Golla Likhitha <golla.likhitha@hcl.com>
2022-06-07 15:51:05 +0530

    added dto classes

--- Eve_Salon/src/main/java/com/eve/salon/requestdto/EveCustomerRequestDto.java
+++ Eve_Salon/src/main/java/com/eve/salon/requestdto/EveCustomerRequestDto.java
@@ -1,5 +1,24 @@
 package com.eve.salon.requestdto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EveCustomerRequestDto {
+	
+	private String eveCustomerName;
+	private Integer eveCustomerPhone;
+	private Integer eveCustomerAlternatePhone;
+	private String eveCustomerEmail;
+	private String eveCustomerGender;
+	private String eveCustomerGroup;
+	private LocalDate eveCustomerBirthday;
+	private LocalDate eveCustomerAnniversary;
+	private String eveCustomerNotes;
 
 }
--- Eve_Salon/src/main/java/com/eve/salon/responsedto/EveCustomerResponseDto.java
+++ Eve_Salon/src/main/java/com/eve/salon/responsedto/EveCustomerResponseDto.java
@@ -1,5 +1,24 @@
 package com.eve.salon.responsedto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EveCustomerResponseDto {
+	private Integer eveCustomerId;
+	private String eveCustomerName;
+	private Integer eveCustomerPhone;
+	private Integer eveCustomerAlternatePhone;
+	private String eveCustomerEmail;
+	private String eveCustomerGender;
+	private String eveCustomerGroup;
+	private LocalDate eveCustomerBirthday;
+	private LocalDate eveCustomerAnniversary;
+	private String eveCustomerNotes;
 
 }
