c390fb1d64d568323ecbf2dd595f3453d7a244d6
sai kiran <you@example.com>
2022-06-07 11:48:47 +0530

    Added Entity Class

---
+++ Eve_Salon/src/main/java/com/eve/salon/entity/EveCustomerInformation.java
@@ -0,0 +1,32 @@
+package com.eve.salon.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "eve_customer_information")
+public class EveCustomerInformation {
+	@Column(name = "eve_customer_id")
+	private Integer eveCustomerId;
+	@Column(name = "eve_customer_name")
+	private String eveCustomerName;
+	@Column(name = "eve_customer_phone")
+	private Integer eveCustomerPhone;
+	@Column(name = "eve_customer_alternate_phone")
+	private Integer eveCustomerAlternatePhone;
+	@Column(name = "eve_customer_email")
+	private String eveCustomerEmail;
+	@Column(name = "eve_customer_gender")
+	private String eveCustomerGender;
+	@Column(name = "eve_customer_group")
+	private String eveCustomerGroup;
+	@Column(name = "eve_customer_birthday")
+	private LocalDate eveCustomerBirthday;
+	@Column(name = "eve_customer_anniversary")
+	private LocalDate eveCustomerAnniversary;
+	@Column(name = "eve_customer_notes")
+	private String eveCustomerNotes;
+}
