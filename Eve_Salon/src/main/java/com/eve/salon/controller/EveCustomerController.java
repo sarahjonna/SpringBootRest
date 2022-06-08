01884bc8fe46d0d8a954f1a831e9598fb346bcf9
sai kiran <you@example.com>
2022-06-08 16:00:04 +0530

    created update,delete and retrieve api

--- Eve_Salon/src/main/java/com/eve/salon/controller/EveCustomerController.java
+++ Eve_Salon/src/main/java/com/eve/salon/controller/EveCustomerController.java
@@ -1,10 +1,16 @@
 package com.eve.salon.controller;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +34,24 @@ public class EveCustomerController {
 		EveCustomerInformation eveCustomerInformation = eveCustomerService.addCustomer(eveCustomerRequestDto);
 		BeanUtils.copyProperties(eveCustomerInformation, eveCustomerResponseDto);
 		return new ResponseEntity<EveCustomerResponseDto>(eveCustomerResponseDto, HttpStatus.OK);
+	}
+
+	@PutMapping("/updateCustomer/{id}")
+	public EveCustomerInformation updateCustomer(@PathVariable Integer id,
+			@RequestBody EveCustomerRequestDto evecustRequestDto) {
+		return eveCustomerService.updateCustomer(id, evecustRequestDto);
+	}
+
+	@DeleteMapping("/deleteCustomer/{eveCustomerId}")
+	private String deleteCustomerById(@PathVariable("eveCustomerId") int eveCustomerId) {
+		eveCustomerService.deleteCustomerById(eveCustomerId);
+		return "DELETED CUSTOMER";
+	}
+
+	@GetMapping("/getAllCustomers")
+	public ResponseEntity<List<EveCustomerInformation>> getAllCustomers() {
+		List<EveCustomerInformation> eveCustomerInformation = eveCustomerService.fetchCustomersList();
+		return new ResponseEntity<List<EveCustomerInformation>>(eveCustomerInformation, HttpStatus.OK);
 
 	}
 
--- Eve_Salon/src/main/java/com/eve/salon/service/EveCustomerService.java
+++ Eve_Salon/src/main/java/com/eve/salon/service/EveCustomerService.java
@@ -1,6 +1,8 @@
 package com.eve.salon.service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.stereotype.Service;
 import com.eve.salon.entity.EveCustomerInformation;
 import com.eve.salon.repository.EveCustomerRepository;
 import com.eve.salon.requestdto.EveCustomerRequestDto;
-import com.eve.salon.responsedto.EveCustomerResponseDto;
 
 @Service
 public class EveCustomerService {
@@ -21,10 +22,31 @@ public class EveCustomerService {
 		EveCustomerInformation eveCustomerInformation = new EveCustomerInformation();
 		BeanUtils.copyProperties(customerRequestDto, eveCustomerInformation);
 		eveCustomerInformation.setEveCustomerBirthday(LocalDate.parse(customerRequestDto.getEveCustomerBirthday()));
-		eveCustomerInformation.setEveCustomerAnniversary(LocalDate.parse(customerRequestDto.getEveCustomerAnniversary()));
+		eveCustomerInformation
+				.setEveCustomerAnniversary(LocalDate.parse(customerRequestDto.getEveCustomerAnniversary()));
 		eveCustomerInformation = eveCustomerRepository.save(eveCustomerInformation);
 		return eveCustomerInformation;
 
+	}
+
+	public EveCustomerInformation updateCustomer(Integer id, EveCustomerRequestDto evecustRequestDto) {
+		EveCustomerInformation evecustomer = new EveCustomerInformation();
+		Optional<EveCustomerInformation> optCustomer = eveCustomerRepository.findById(id);
+		if (optCustomer.isPresent()) {
+			evecustomer = optCustomer.get();
+		}
+		BeanUtils.copyProperties(evecustRequestDto, evecustomer);
+		evecustomer.setEveCustomerBirthday(LocalDate.parse(evecustRequestDto.getEveCustomerBirthday()));
+		evecustomer.setEveCustomerAnniversary(LocalDate.parse(evecustRequestDto.getEveCustomerAnniversary()));
+		return evecustomer = eveCustomerRepository.save(evecustomer);
+	}
+
+	public void deleteCustomerById(Integer id) {
+		eveCustomerRepository.deleteById(id);
+	}
+
+	public List<EveCustomerInformation> fetchCustomersList() {
+		return eveCustomerRepository.findAll();
 	}
 
 }
