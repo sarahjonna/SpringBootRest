7593babe2eac4bd7d179a79b332e5e78dc5f0fef
Nisha1999 <s.nisha@hcl.com>
2022-06-13 14:22:34 +0530

    added security config files

--- Eve_Salon/src/main/java/com/eve/salon/jwt/security/repository/UserRepository.java
+++ Eve_Salon/src/main/java/com/eve/salon/jwt/security/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.eve.salon.jwt.security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eve.salon.jwt.security.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
 
 }
