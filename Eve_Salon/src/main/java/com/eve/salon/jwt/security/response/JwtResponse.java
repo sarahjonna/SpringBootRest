64b9f2bb34e609339e349c925c0d3b1d30fc4f9f
sarahjonna <100998930+sarahjonna@users.noreply.github.com>
2022-06-13 14:49:45 +0530

    Added response objects

---
+++ Eve_Salon/src/main/java/com/eve/salon/jwt/security/response/JwtResponse.java
@@ -0,0 +1,26 @@
+package com.eve.salon.jwt.security.response;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class JwtResponse {
+  private String token;
+  private String type = "Bearer";
+  private Long id;
+  private String username;
+  private String email;
+  private List<String> roles;
+
+  public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+    this.token = accessToken;
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.roles = roles;
+  }
+
+}
