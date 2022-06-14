abcc274308cad9331c23965d9bb73159f94a111c
sai kiran <you@example.com>
2022-06-14 17:29:15 +0530

    created authcontroller api's

--- Eve_Salon/src/main/java/com/eve/salon/jwt/security/controller/AuthController.java
+++ Eve_Salon/src/main/java/com/eve/salon/jwt/security/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.eve.salon.jwt.security.controller;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +26,10 @@ import com.eve.salon.jwt.security.repository.RoleRepository;
 import com.eve.salon.jwt.security.repository.UserRepository;
 import com.eve.salon.jwt.security.request.LoginRequest;
 import com.eve.salon.jwt.security.request.SignupRequest;
+import com.eve.salon.jwt.security.response.JwtResponse;
 import com.eve.salon.jwt.security.response.MessageResponse;
+import com.eve.salon.jwt.security.service.UserDetailsImpl;
+import com.eve.salon.jwt.security.utils.JwtUtils;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -32,17 +43,27 @@ public class AuthController {
 	
 	@Autowired
 	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	JwtUtils jwtUtils;
 
 	
 	@PostMapping("/signin")
-	public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
-
-		
-		return (ResponseEntity<?>) ResponseEntity.ok();
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+		Authentication authentication= authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()) );
+         SecurityContextHolder.getContext().setAuthentication(authentication);
+         UserDetailsImpl userDetails =  (UserDetailsImpl) authentication.getPrincipal();
+         List<String> roles= userDetails.getAuthorities().stream().map(item-> item.getAuthority()).collect(Collectors.toList());
+         String token=jwtUtils.generateJwtToken(authentication);
+         
+		return ResponseEntity.ok(new JwtResponse(token, userDetails.getId(), userDetails.getUsername(), userDetails.getPassword(), roles));
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
 		if(userRepository.existsByUsername(signupRequest.getUsername())) {
 			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken"));
 		}
